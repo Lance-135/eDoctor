7e1abaf81b73ec7806ca59e364d2b52a0a8c8e71
Lance <austinkarki84@gmail.com>
2025-06-12 23:44:48 +0545

    final commit

--- e_doctor/e_doctor/settings.py
+++ e_doctor/e_doctor/settings.py
@@ -37,11 +37,13 @@ ALLOWED_HOSTS = [
 CORS_ALLOW_CREDENTIALS = True
 # CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = [
+    'https://e-doctor-livid.vercel.app',
     'http://localhost:5173',
 ]
 # Csrf trusted origins
 CSRF_TRUSTED_ORIGINS = [
     'https://edoctor.onrender.com',
+    'https://e-doctor-livid.vercel.app',
     'http://localhost:5173',
     'http://127.0.0.1:8000/'
 ]
